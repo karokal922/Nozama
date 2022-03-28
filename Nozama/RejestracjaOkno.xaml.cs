52ebc789c24f1ed7ad4c9afd3364f32e9e96a4e0
Karol Kalita <karokal922@student.polsl.pl>
2022-03-28 19:24:39 +0200

    RejestracjaOkno

--- Nozama/RejestracjaOkno.xaml.cs
+++ Nozama/RejestracjaOkno.xaml.cs
@@ -23,5 +23,10 @@ namespace Nozama
         {
             InitializeComponent();
         }
+
+        private void btnLoguj_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
