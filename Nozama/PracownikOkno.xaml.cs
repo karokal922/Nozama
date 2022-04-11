4d497bdca4a5729f05d7c526a3be7d6ff4c45f82
Karol Kalita <karokal922@student.polsl.pl>
2022-04-11 20:33:56 +0200

    pracownikOkno AkceptacjaSkonczona

--- Nozama/PracownikOkno.xaml.cs
+++ Nozama/PracownikOkno.xaml.cs
@@ -22,6 +22,7 @@ namespace Nozama
     public partial class PracownikOkno : Window
     {
         private DataTable dostepneZLEC = new DataTable();
+        private DataTable zaakceptowaneZLEC = new DataTable();
         private int IDKuriera;
         public PracownikOkno()
         {
@@ -49,14 +50,19 @@ namespace Nozama
             MySqlDataReader dataReader = command.ExecuteReader();
             dataReader.Read();
             IDKuriera = (int)dataReader.GetValue(0);
+            dataReader.Close();
+            MySqlCommand command1 = new MySqlCommand($"SELECT zamowienie.ID_Zamowienia,ad1.Miejscowosc,ad2.Miejscowosc,paczka.Wysokosc,paczka.Szerokosc,paczka.Glebokosc,paczka.Waga,status.Status,zamowienie.Cena FROM zamowienie,klienci as kl1,klienci as kl2,adres as ad1,adres as ad2,paczka,aktualny_status,status WHERE zamowienie.Paczka_ID=paczka.ID_Paczki AND zamowienie.Nadawca_ID=kl1.ID_Klienta AND kl1.Adres_ID=ad1.ID_Adresu AND zamowienie.Odbiorca_ID=kl2.ID_Klienta AND kl2.Adres_ID=ad2.ID_Adresu AND zamowienie.ID_Zamowienia=aktualny_status.Zamowienia_ID AND aktualny_status.Status_ID!=1 AND aktualny_status.Status_ID!=5 and zamowienie.Kurier_ID={IDKuriera} AND aktualny_status.Status_ID=status.ID_Statusu; ", MainWindow.contact.connection);
+            command1.ExecuteNonQuery();
+            MySqlDataAdapter adapter1 = new MySqlDataAdapter(command1);
+            adapter1.Fill(zaakceptowaneZLEC);
+            dtaPrzyjeteZlecenia.ItemsSource = zaakceptowaneZLEC.DefaultView;
             MainWindow.contact.connection.Close();
         }
         private void btnWyloguj_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        public void UstawTytułyKolumn()
         {
             dtaDostepneZlecenia.Columns[0].Header = " ID ";
             dtaDostepneZlecenia.Columns[1].Header = " Z kąd ";
@@ -65,6 +71,20 @@ namespace Nozama
             dtaDostepneZlecenia.Columns[4].Header = " Szerokość ";
             dtaDostepneZlecenia.Columns[5].Header = " Głębokość ";
             dtaDostepneZlecenia.Columns[6].Header = " Waga ";
+
+            dtaPrzyjeteZlecenia.Columns[0].Header = " ID ";
+            dtaPrzyjeteZlecenia.Columns[1].Header = " Z kąd ";
+            dtaPrzyjeteZlecenia.Columns[2].Header = " Dokąd ";
+            dtaPrzyjeteZlecenia.Columns[3].Header = " Długość ";
+            dtaPrzyjeteZlecenia.Columns[4].Header = " Szerokość ";
+            dtaPrzyjeteZlecenia.Columns[5].Header = " Głębokość ";
+            dtaPrzyjeteZlecenia.Columns[6].Header = " Waga ";
+            dtaPrzyjeteZlecenia.Columns[7].Header = " Status ";
+            dtaPrzyjeteZlecenia.Columns[8].Header = " Cena ";
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            UstawTytułyKolumn();
         }
 
         private void btnZmienStatus_Click(object sender, RoutedEventArgs e)
@@ -115,6 +135,16 @@ namespace Nozama
             cenaKoncowa += (decimal)((x * y * z / 1000000) * cenaZaMetrSzescienny);
             return cenaKoncowa;
         }
+        public void CzyscDostepneZleceniaGrid()
+        {
+            DataTable empty = new DataTable();
+            dtaDostepneZlecenia.ItemsSource = empty.DefaultView;
+        }
+        public void CzyscPrzyjeteZleceniaGrid()
+        {
+            DataTable empty = new DataTable();
+            dtaPrzyjeteZlecenia.ItemsSource = empty.DefaultView;
+        }
         private void btnZaakceptuj_Click(object sender, RoutedEventArgs e)
         {
             try
@@ -135,10 +165,32 @@ namespace Nozama
                     double waga = (double)dostepneZLEC.Rows[zaznaczonyIndex][6];
                     decimal cenaZamowienia = Math.Floor(ObliczCeneDostawy(wymiarX, wymiarY, wymiarZ, waga, losowyDystans));
                     MainWindow.contact.connection.Open();
+
                     MySqlCommand command = new MySqlCommand($"UPDATE `zamowienie` SET `Kurier_ID` = '{IDKuriera}', `Dystans` = '{losowyDystans}', `Cena` = '{cenaZamowienia}' WHERE `zamowienie`.`ID_Zamowienia` = {idZaznaczonegoZamowienia}; ", MainWindow.contact.connection);
                     command.ExecuteNonQuery();
+                    command = new MySqlCommand($"UPDATE aktualny_status SET aktualny_status.Status_ID=4 WHERE aktualny_status.Zamowienia_ID={idZaznaczonegoZamowienia}", MainWindow.contact.connection);
+                    command.ExecuteNonQuery();
+
+                    CzyscPrzyjeteZleceniaGrid();
+                    CzyscDostepneZleceniaGrid();
+
+                    command = new MySqlCommand($"SELECT zamowienie.ID_Zamowienia,ad1.Miejscowosc,ad2.Miejscowosc,paczka.Wysokosc,paczka.Szerokosc,paczka.Glebokosc,paczka.Waga,status.Status,zamowienie.Cena FROM zamowienie,klienci as kl1,klienci as kl2,adres as ad1,adres as ad2,paczka,aktualny_status,status WHERE zamowienie.Paczka_ID=paczka.ID_Paczki AND zamowienie.Nadawca_ID=kl1.ID_Klienta AND kl1.Adres_ID=ad1.ID_Adresu AND zamowienie.Odbiorca_ID=kl2.ID_Klienta AND kl2.Adres_ID=ad2.ID_Adresu AND zamowienie.ID_Zamowienia=aktualny_status.Zamowienia_ID AND aktualny_status.Status_ID!=1 AND aktualny_status.Status_ID!=5 and zamowienie.Kurier_ID={IDKuriera} AND aktualny_status.Status_ID=status.ID_Statusu; ", MainWindow.contact.connection);
+                    command.ExecuteNonQuery();
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                    zaakceptowaneZLEC.Clear();
+                    adapter.Fill(zaakceptowaneZLEC);
+                    dtaPrzyjeteZlecenia.ItemsSource = zaakceptowaneZLEC.DefaultView;
+
+                    command = new MySqlCommand("SELECT zamowienie.ID_Zamowienia,ad1.Miejscowosc,ad2.Miejscowosc,paczka.Wysokosc,paczka.Szerokosc,paczka.Glebokosc,paczka.Waga FROM zamowienie,klienci as kl1,klienci as kl2,adres as ad1,adres as ad2,paczka,aktualny_status WHERE zamowienie.Paczka_ID=paczka.ID_Paczki AND zamowienie.Nadawca_ID=kl1.ID_Klienta AND kl1.Adres_ID=ad1.ID_Adresu AND zamowienie.Odbiorca_ID=kl2.ID_Klienta AND kl2.Adres_ID=ad2.ID_Adresu AND zamowienie.ID_Zamowienia=aktualny_status.Zamowienia_ID AND aktualny_status.Status_ID=1;", MainWindow.contact.connection);
+                    command.ExecuteNonQuery();
+                    MySqlDataAdapter adapter1 = new MySqlDataAdapter(command);
+                    dostepneZLEC.Clear();
+                    adapter1.Fill(dostepneZLEC);
+                    dtaDostepneZlecenia.ItemsSource = dostepneZLEC.DefaultView;
+
+                    UstawTytułyKolumn();
+
                     MainWindow.contact.connection.Close();
-                    MessageBox.Show(cenaZamowienia.ToString());
                 }
             }
             catch (InvalidOperationException exception)
@@ -149,6 +201,12 @@ namespace Nozama
             {
                 MessageBox.Show(exception.Message);
             }
+        }
+
+        private void btnTest_Click(object sender, RoutedEventArgs e)
+        {
+            CzyscPrzyjeteZleceniaGrid();
+            CzyscDostepneZleceniaGrid();
         }
     }
 }
